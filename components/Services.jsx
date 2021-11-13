663db17db8ad0ed4a6c7bafb2ba2e34196f67b78
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-11-12 22:17:45 -0500

    Initial commit

---
+++ components/Services.jsx
@@ -0,0 +1,13 @@
+import styles from "../styles/Services.module.css";
+
+const Services = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}></h1>
+      <h1 className={styles.sub}></h1>
+      <div className={styles.services}></div>
+    </div>
+  );
+};
+
+export default Services;
