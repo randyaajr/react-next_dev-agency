f9eaa54f60cfee65efe27b1b24d4a2c3c546d4ae
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-11-15 18:40:31 -0500

    Created and styled footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import styles from "../styles/Footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardL}>
+        <h1 className={styles.title}>Randycodes</h1>
+        <h1 className={styles.linkTitle}>
+          <a href="/contact" className={styles.link} passHref>
+            <>
+              <span className={styles.linkText}>WORK WITH US</span>
+              <Image src="/images/link.png" width="40px" height="40px" alt="" />
+            </>
+          </a>
+        </h1>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          Toronto, Ontario
+          <br /> Canada
+        </div>
+        <div className={styles.cardItem}>
+          CONTACT@randycodes.ca
+          <br /> 647_773_8434
+        </div>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          FOLLOW US:
+          <br /> __FB __IN __BE __TW
+        </div>
+        <div className={styles.cardItem}>
+          © 2022 Randy Anderson,
+          <br />
+          ALL RIGHTS RESERVED
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
